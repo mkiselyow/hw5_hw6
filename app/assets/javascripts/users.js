f9b96e280e2d8628bef6684291cbba8f11b7a5f1
MaximKiselyow <m.kiselyow@gmail.com>
2017-04-14 04:16:09 +0300

    fadingOut_notice_after_SignUp

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,29 @@
+$(document).ready(function(){
+  $("body").on("click", "h2", function(){
+    alert("CLICKED");
+  });
+  // $("#flash_notice").on({
+  //   click: function() {
+  //     $(this).css("background-color", "pink");
+  //   },
+  //   click : function() {
+  //     $(this).fadeOut(5000);
+  //   }
+  // });
+
+  setTimeout(function(){$("#flash_notice").fadeOut(6000)},3000);
+  
+
+  $("body").on("click", "#list_toggle", function(e){
+    // list = $('.user_list');
+    // list.fadeIn();
+    text = $(e.target).text();
+    text = text === "Hide" ? "Show" : "Hide";
+    $(e.target).text(text);
+
+    return false;
+  });
+});
 
 
 
